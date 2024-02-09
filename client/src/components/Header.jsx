db5beb39eef5278bb8b7889058b1999b91be0767
Tanushree227 <tanushreedas813@gmail.com>
2024-02-09 11:50:44 +0530

    font changed

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -34,7 +34,7 @@ export default function Header() {
           <FaMoon />
         </Button>
         <Link to="/sign-in">
-          <Button gradientDuoTone="cyanToBlue" pill>
+          <Button gradientDuoTone="cyanToBlue" className="text-lg" pill>
             Sign In
           </Button>
         </Link>
@@ -42,13 +42,13 @@ export default function Header() {
       </div>
         <Navbar.Collapse>
             <Navbar.Link active={path === '/'} as={'div'}>
-                <Link to='/'>Home</Link>
+                <Link to='/' className="text-lg">Home</Link>
             </Navbar.Link>
             <Navbar.Link active={path === '/about'} as={'div'}>
-                <Link to='/about'>About</Link>
+                <Link to='/about' className="text-lg">About</Link>
             </Navbar.Link>
             <Navbar.Link active={path === '/projects'} as={'div'}>
-                <Link to='/projects'>Projects</Link>
+                <Link to='/projects' className="text-lg">Projects</Link>
             </Navbar.Link>
         </Navbar.Collapse>
     </Navbar>
